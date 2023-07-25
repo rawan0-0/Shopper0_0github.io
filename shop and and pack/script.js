791149316c20d0efa20ca45e5443433696c9fd3e
rawan0-0 <128239109+rawan0-0@users.noreply.github.com>
2023-07-25 15:14:53 +0100

    Add files via upload

---
+++ shop and and pack/script.js
@@ -0,0 +1,127 @@
+// script.js
+
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const dropdown = document.querySelectorAll(".dropdown");
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.querySelector(".menu");
+const links = document.querySelectorAll(".dropdown a");
+
+function setAriaExpandedFalse() {
+  dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
+}
+
+function closeDropdownMenu() {
+  dropdown.forEach((drop) => {
+    drop.classList.remove("active");
+    drop.addEventListener("click", (e) => e.stopPropagation());
+  });
+}
+
+function toggleHamburger() {
+  navMenu.classList.toggle("show");
+}
+
+dropdownBtn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const dropdownIndex = e.currentTarget.dataset.dropdown;
+    const dropdownElement = document.getElementById(dropdownIndex);
+
+    dropdownElement.classList.toggle("active");
+    dropdown.forEach((drop) => {
+      if (drop.id !== btn.dataset["dropdown"]) {
+        drop.classList.remove("active");
+      }
+    });
+    e.stopPropagation();
+    btn.setAttribute(
+      "aria-expanded",
+      btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
+    );
+  });
+});
+
+// close dropdown menu when the dropdown links are clicked
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+    toggleHamburger();
+  })
+);
+
+// close dropdown menu when you click on the document body
+document.documentElement.addEventListener("click", () => {
+  closeDropdownMenu();
+  setAriaExpandedFalse();
+});
+
+// close dropdown when the escape key is pressed
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+  }
+});
+
+// toggle hamburger menu
+hamburgerBtn.addEventListener("click", toggleHamburger);
+
+// Function to add a product to the shopping basket
+function addToBasket(productName) {
+  var basketItems = document.getElementById('basket-items');
+  var newItem = document.createElement('li');
+  newItem.textContent = productName;
+  basketItems.appendChild(newItem);
+}
+
+// Function to clear all items from the shopping basket
+function clearBasket() {
+  var basketItems = document.getElementById('basket-items');
+  basketItems.innerHTML = '';
+}
+
+// Example usage of addToBasket
+const addToBasketButton = document.getElementById('add-to-basket');
+addToBasketButton.addEventListener('click', () => {
+  const productName = prompt('Enter the product name:');
+  if (productName) {
+    addToBasket(productName);
+  }
+});
+
+// Example usage of clearBasket
+const clearBasketButton = document.getElementById('clear-basket');
+clearBasketButton.addEventListener('click', () => {
+  clearBasket();
+});
+
+
+<script src="myscripts.js"></script>
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+// images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+}
